aed924d036c692ac8e46ac5c57aada0a74272a5d
rmc29 <rachael.churchill@cantab.net>
2025-05-14 20:56:59 +0100

    tidying, adding more comments

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,15 @@
+/*
+This is a simple dice-rolling app to enforce expected probabilities when rolling two six-sided dice for board games like (Settlers of) Catan and its sequels.
+Rather than rolling each D6 independently (which can cause an overabundance of 2s or 12s, or a shortage of 6s or 8s), it produces all 36 possible pairs once each in a random order (and then starts again if needed).
+*/
+
 import { Text, View, Button, Pressable, StyleSheet } from "react-native";
 import { useState } from 'react';
 
-var rolls = new Array(0); //todo should probably make this state (or context?) as well, not just an ordinary global
+var rolls = new Array(0); //TODO maybe make this state (or context?) as well, rather than just an ordinary global
 
 
-
+/*Populate an array with the 36 possible combinations, and shuffle them*/
 function createRolls() {
 	var i: number;
 	var tmp: number;
@@ -24,7 +29,8 @@ function createRolls() {
 }
 
 
-// Display components
+/* The display components for each die, the sum of their two values, the container for all those, and the roll button
+(TODO These should probably be in their own files) */
 function Die({value}) {
 	return ( 
 		<View style={styles.dieview}>
@@ -39,33 +45,13 @@ function Total({value}) {
 	return ( <Text>Total: {value}</Text>);
 }
 
-function RollButton({setter}) {
-	function rollButtonClickHandler() {
-		
-		if (rolls.length == 0) createRolls();
-		var roll: number = rolls.pop();
-		setter(roll);	
-	}
-
-  return (
-		<Pressable   onPress={rollButtonClickHandler}>
-			<View		style={styles.button} >
-				<Text>Click to roll</Text>
-			</View>
-		</Pressable>
-  );
-}
- 
  function DiceDisplay({roll}) {
-
-	//console.log(roll);
 	if (roll == -1) {
-		return (<Text> </Text>);
+		return (<Text> </Text>); //if the array of 36 rolls hasn't been created yet, return an empty text container
 	}
-	else {
+	else { //extract the two individual die values from the number between 0 and 35
 		var die1:number = (roll % 6) + 1;
-		var die2:number = ((roll - (roll % 6)) / 6) + 1;	
-		
+		var die2:number = ((roll - (roll % 6)) / 6) + 1;			
 		
 		return (
 		<>
@@ -76,17 +62,33 @@ function RollButton({setter}) {
 			<Total value={die1+die2} />
 			</>
 		);
-	}
- 
+	} 
  }
+
+
+function RollButton({setter}) {
+	function rollButtonClickHandler() {
+		
+		if (rolls.length == 0) createRolls(); //if the array of rolls is empty (either because we just started or because they've been used up) create it
+		var roll: number = rolls.pop(); //pop one of the shuffled numbers 0-35 off the array
+		setter(roll);	
+	}
+
+  return (
+		<Pressable onPress={rollButtonClickHandler}>
+			<View style={styles.button} >
+				<Text>Click to roll</Text>
+			</View>
+		</Pressable>
+  );
+}
+ 
+
 
 export default function Catan() {
 	
-	const [roll, setroll] = useState(-1);
+	const [roll, setroll] = useState(-1); //to store state for current roll: initialise to -1
 	
-	//console.log(rolls);
-	
-
   return (
 		<View style={styles.mainView}>
 		<Text>Fair dice app for Catan games</Text>
